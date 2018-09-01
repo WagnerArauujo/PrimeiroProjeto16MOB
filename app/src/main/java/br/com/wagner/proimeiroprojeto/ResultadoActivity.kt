3fd84a87348ad5a1aa0f88f28dcc9a9bd225b2f8
Wagner Araújo <araujo.wagnerp@gmail.com>
2018-09-01 12:13:54 -0300

    Alterações Aula 02

--- app/src/main/java/br/com/wagner/proimeiroprojeto/ResultadoActivity.kt
+++ app/src/main/java/br/com/wagner/proimeiroprojeto/ResultadoActivity.kt
@@ -2,8 +2,10 @@ package br.com.wagner.proimeiroprojeto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 import br.com.wagner.proimeiroprojeto.utils.ConstantesExtra
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
@@ -14,9 +16,50 @@ class ResultadoActivity : AppCompatActivity() {
         val peso = intent.extras.getString(ConstantesExtra.KEY_PESO)
         val altura = intent.extras.getString(ConstantesExtra.KEY_ALTURA)
 
-        Toast.makeText(this,
-                peso,
-                Toast.LENGTH_SHORT).show()
+        calcularIMC(peso.toDouble(), altura.toDouble())
+
 
     }
+
+    private fun calcularIMC(peso: Double, altura: Double) {
+        val imc = peso / (altura * altura)
+        when (imc){
+            in 0..17 -> {
+                tvIMC.setText("magreza")
+                setImage(R.drawable.magreza)
+            }
+            in 17.1..18.5 -> {
+                tvIMC.setText("Abaixo do peso")
+                setImage(R.drawable.abaixo)
+            }
+            in 18.6..24.9 -> {
+                tvIMC.setText("Peso Ideal")
+                setImage(R.drawable.ideal)
+            }
+            in 25.0..29.9 -> {
+                tvIMC.setText("Levemente acima do peso")
+                setImage(R.drawable.sobre)
+            }
+            in 30.0..34.9 -> {
+                tvIMC.setText("Obesidade 1")
+                setImage(R.drawable.obesidade)
+            }
+            in 34.5..39.9 -> {
+                tvIMC.setText("Pbesidade 2")
+                setImage(R.drawable.obesidade)
+            }
+            else -> {
+                tvIMC.setText("Obesidade 3")
+                setImage(R.drawable.obesidade)
+            }
+        }
+    }
+
+    fun setImage(resourceID: Int) {
+        ivIMC.setImageDrawable(ContextCompat.getDrawable(this, resourceID))
+    }
+
 }
+
+
+
